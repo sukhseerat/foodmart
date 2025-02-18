262b4b69d6807a54a09d778d54e265124d265c7e
list <sukhseeratsokhi@gmail.com>
2025-02-18 23:16:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Header from './components/Header';
+import Category from './Components/Category';
+import Newly from './components/Newly';
+import TrendingProduct from "./components/TrendingProduct";
+import PromoCard from "./components/PromoCard";
+import BestSelling from "./components/BestSelling";
+import DiscountForm from "../src/components/DiscountForm";
+import ProductList from "../src/components/ProductList";
+import JustArrived from "../src/components/JustArrived";
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Category/>
+      <Newly/>
+      <TrendingProduct/>
+      <PromoCard/>
+      <BestSelling/>
+      <DiscountForm/>
+     <ProductList/>
+      <JustArrived/>
+     
+     
+    </div>
+  );
+};
+ 
+export default App;
+ 
